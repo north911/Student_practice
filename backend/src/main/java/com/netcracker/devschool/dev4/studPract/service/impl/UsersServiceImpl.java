0c8f89807329b3a25016a0f821a2e0f13f65b9cb
Andrei Danilenko <north50911@gmail.com>
2017-12-18 10:21:04 +0300

    fixes(2)

--- backend/src/main/java/com/netcracker/devschool/dev4/studPract/service/impl/UsersServiceImpl.java
+++ backend/src/main/java/com/netcracker/devschool/dev4/studPract/service/impl/UsersServiceImpl.java
@@ -39,7 +39,7 @@ public class UsersServiceImpl implements UsersService {
     @Override
     public UsersEntity saveUser(UsersEntity user, UserRolesEntity userRole) {
 
-        if(userRolesRepository.findByUsername(user.getusername())!=null){
+        if(userRolesRepository.findByUsername(user.getusername())==null){
         userRolesRepository.save(userRole);
         return usersRepository.save(user);}
         else return null;
